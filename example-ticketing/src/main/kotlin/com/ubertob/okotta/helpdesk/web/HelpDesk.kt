1b01413c617d45bcbbdec0d1450573e826577aa9
Asad Manji <asad.manji@gmail.com>
2020-09-28 13:19:00 -0400

    Connect up http handler for getting a ticket

--- example-ticketing/src/main/kotlin/com/ubertob/okotta/helpdesk/web/HelpDesk.kt
+++ example-ticketing/src/main/kotlin/com/ubertob/okotta/helpdesk/web/HelpDesk.kt
@@ -4,14 +4,16 @@ import com.ubertob.okotta.helpdesk.domain.CommandAddToBacklog
 import com.ubertob.okotta.helpdesk.domain.TicketCommandHandler
 import com.ubertob.okotta.helpdesk.domain.TicketsProjection
 import org.http4k.core.*
+import org.http4k.core.Status.Companion.OK
 import org.http4k.routing.bind
+import org.http4k.routing.path
 import org.http4k.routing.routes
 
 
 class HelpDesk(val ticketsProjection: TicketsProjection, val commandHandler: TicketCommandHandler): HttpHandler {
 
     val httpHandler = routes(
-        "/ping" bind Method.GET to { Response(Status.OK).body("pong") },
+        "/ping" bind Method.GET to { Response(OK).body("pong") },
         "/ticket/{ticketId}" bind Method.GET to ::getTicket,
         "/ticket" bind Method.POST to ::addTicket
     )
@@ -20,11 +22,13 @@ class HelpDesk(val ticketsProjection: TicketsProjection, val commandHandler: Tic
         val add: AddTicketRequest = request.bodyString().deserialise()
         return commandHandler(CommandAddToBacklog(add.title, add.description))
             .first().entityKey
-            .let { Response(Status.OK).body(AddTicketResponse(it).serialise()) }
+            .let { Response(OK).body(AddTicketResponse(it).serialise()) }
     }
 
     private fun getTicket(request: Request): Response {
-        return TODO("not implemented")
+        val ticketId = request.path("ticketId") ?: return Response(Status.BAD_REQUEST)
+        val found = ticketsProjection.getTicket(ticketId) ?: return Response(Status.NOT_FOUND)
+        return Response(OK).body(GetTicketResponse(found.title, found.description, found.kanbanColumn.name).serialise())
     }
 
     override fun invoke(req: Request): Response  =
@@ -32,5 +36,7 @@ class HelpDesk(val ticketsProjection: TicketsProjection, val commandHandler: Tic
 
 }
 
+// these types define the property names of the serialised json request/response objects
 data class AddTicketRequest(val title: String, val description: String): JsonSerialisable
 data class AddTicketResponse(val id: String): JsonSerialisable
+data class GetTicketResponse(val title: String, val description: String, val kanban_column: String): JsonSerialisable
--- example-ticketing/src/test/kotlin/com/ubertob/okotta/helpdesk/web/HelpDeskTest.kt
+++ example-ticketing/src/test/kotlin/com/ubertob/okotta/helpdesk/web/HelpDeskTest.kt
@@ -1,7 +1,10 @@
 package com.ubertob.okotta.helpdesk.web
 
+import com.ubertob.okotta.helpdesk.domain.CommandAddToBacklog
+import org.http4k.core.Method.GET
 import org.http4k.core.Method.POST
 import org.http4k.core.Request
+import org.http4k.core.Status.Companion.NOT_FOUND
 import org.http4k.core.Status.Companion.OK
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
@@ -12,7 +15,7 @@ import strikt.assertions.isNotNull
 internal class HelpDeskTest {
 
     @Test
-    fun `create a new ticket and retrieve it`() {
+    fun `create a new ticket`() {
         val handler = helpDeskBuilder()
 
         val response = handler(
@@ -27,6 +30,31 @@ internal class HelpDeskTest {
         expectThat(response.status).isEqualTo(OK)
         val jsonContent = response.bodyAsJson()
         expectThat(jsonContent["id"]).isNotNull()
+    }
+
+    @Test
+    fun `retrieve a ticket`() {
+        val handler = helpDeskBuilder()
+
+        // setup: create a ticket
+        val id = handler.commandHandler(CommandAddToBacklog("title1", "description1")).first().entityKey
+
+        val response = handler(Request(GET, "/ticket/$id"))
+
+        expectThat(response.status).isEqualTo(OK)
+        val jsonContent = response.bodyAsJson()
+        expectThat(jsonContent["title"].asText()).isEqualTo("title1")
+        expectThat(jsonContent["description"].asText()).isEqualTo("description1")
+        expectThat(jsonContent["kanban_column"].asText()).isEqualTo("Backlog")
+    }
+
+    @Test
+    fun `retrieve a ticket error handling`() {
+        val handler = helpDeskBuilder()
+
+        handler(Request(GET, "/ticket/id_not_exists")).run {
+            expectThat(status).isEqualTo(NOT_FOUND)
+        }
     }
 }
 
