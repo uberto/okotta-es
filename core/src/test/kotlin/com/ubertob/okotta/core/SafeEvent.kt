a9296b7f6eb8360d275d170e621150d578d7db4a
Asad Manji <asad.manji@gmail.com>
2020-10-08 17:09:53 +0100

    Exercise 1 solution

--- core/src/test/kotlin/com/ubertob/okotta/core/SafeEvent.kt
+++ core/src/test/kotlin/com/ubertob/okotta/core/SafeEvent.kt
@@ -28,20 +28,38 @@ object InitialState : SafeState() {
 data class Open internal constructor(
         val id: SafeId
 ) : SafeState() {
-    override fun combine(event: SafeEvent): SafeState = TODO()
+    override fun combine(event: SafeEvent): SafeState =
+        when (event) {
+            is Closed -> AlarmInactive(event.id)
+            is Opened,
+            is Locked,
+            is Unlocked -> InvalidState(event.id, this, event)
+        }
 }
 
 data class AlarmInactive internal constructor(
         val id: SafeId
 ) : SafeState() {
-    override fun combine(event: SafeEvent): SafeState = TODO()
+    override fun combine(event: SafeEvent): SafeState =
+        when (event) {
+            is Opened -> Open(event.id)
+            is Locked -> AlarmActive(event.id, event.code)
+            is Closed,
+            is Unlocked -> InvalidState(event.id, this, event)
+        }
 }
 
 data class AlarmActive internal constructor(
         val id: SafeId,
         val code: String
 ) : SafeState() {
-    override fun combine(event: SafeEvent): SafeState = TODO()
+    override fun combine(event: SafeEvent): SafeState =
+        when (event) {
+            is Unlocked -> AlarmInactive(event.id)
+            is Opened,
+            is Closed,
+            is Locked -> InvalidState(event.id, this, event)
+        }
 }
 
 data class InvalidState(val id: SafeId, val prevState: SafeState, val event: SafeEvent): SafeState() {
