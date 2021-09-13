b16c30d486e96ce1757d467e69cf13e54c65a654
uberto <uberto.gama@gmail.com>
2021-09-13 07:39:46 +0100

    tidy up
    Signed-off-by: uberto <uberto.gama@gmail.com>

--- example-ticketing/src/main/kotlin/com/ubertob/okotta/helpdesk/domain/TicketEventStore.kt
+++ example-ticketing/src/main/kotlin/com/ubertob/okotta/helpdesk/domain/TicketEventStore.kt
@@ -3,5 +3,7 @@ package com.ubertob.okotta.helpdesk.domain
 import com.ubertob.okotta.helpdesk.lib.EventStore
 import com.ubertob.okotta.helpdesk.lib.EventStoreInMemory
 
-class TicketEventStore : EventStore<TicketEvent> by EventStoreInMemory()
+class TicketEventStore : EventStore<TicketEvent> by EventStoreInMemory() {
+    //todo add fetchByTicketName
+}
 
